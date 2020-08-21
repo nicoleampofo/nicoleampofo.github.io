1ba59ea17282cdd9afae32534db727f29c552973
Nicole Ampofo <30731402+nicoleampofo@users.noreply.github.com>
2020-08-21 14:42:28 -0400

    Add files via upload

---
+++ AboutMe.js
@@ -0,0 +1,8 @@
+function clickFunction() {
+	var x = document.getElementById("toggle");
+  	if (x.style.display === "block") {
+    	x.style.display = "none";
+  } else {
+   	x.style.display = "block";
+  }
+}
